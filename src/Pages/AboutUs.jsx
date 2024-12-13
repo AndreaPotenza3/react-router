8aa4785a8304e170d16a36d08f1f528fe26c5aaa
AndreaPotenza <andreapoten3@gmail.com>
2024-12-13 20:04:19 +0100

    completo da perfezionare..quasi 0 css

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,5 +1,8 @@
 export default function About() {
     return (
-        <h1>About</h1>
+        <>
+            <h1 className="sub-title">Riot Games</h1>
+            <p className="about-description">Riot Games è stata fondata nel 2006 con l'obiettivo di creare giochi ed esperienze che rendano migliore l'avventura di essere un giocatore. Da allora, siamo cresciuti fino ad avere più di 4500 Rioter in oltre 20 uffici in tutto il mondo, che offrono una prospettiva globale ai nostri giochi e ai personaggi che li popolano. Dalle strade di Piltover ai Laboratori di radianite su Terra Alfa, il nostro obiettivo è creare giochi ed essere al servizio delle persone a cui piacciono.</p>
+        </>
     )
 }
--- src/Pages/List.jsx
+++ src/Pages/List.jsx
@@ -3,7 +3,7 @@ import { characters } from '../Data/characters.js'
 export default function List() {
     return (
         <>
-            <h1 className='list-title'>Characters List</h1>
+            <h1 className='sub-title'>Characters List</h1>
             <ul className='char-list'>
                 {characters.map(char => (
                     <li key={char.id}>
